08d4f227c440bea1e1efd12a7cdcf5c6a67ca736
Vijay Ganesh H <vijay.g1@samsung.com>
2020-11-06 10:01:24 +0530

    First commit

---
+++ src/errors.js
@@ -0,0 +1,41 @@
+/**
+ * Generic not found error for slot based search queries
+ */
+class SlotNotFoundError extends Error {}
+
+/**
+ * Raise when an occupied slot is attempted to be assigned to another car
+ */
+class SlotNotVacantError extends Error {
+  constructor(id, message) {
+    super(message);
+
+    this.slot_id = id;
+    this.message = message;
+  }
+}
+
+/**
+ * Raised when parking lot is full and more cars cannot be parked
+ */
+class ParkingLotFullError extends Error {}
+
+/**
+ * Raised when an function gets invalid/bad data
+ */
+class BadDataError extends Error {}
+
+
+// raised when car with same registration and color enters again
+class InvalidRequestError extends Error {}
+
+class SlotAlreadyVacantError extends Error {}
+
+module.exports = {
+  SlotNotFoundError,
+  SlotNotVacantError,
+  ParkingLotFullError,
+  BadDataError,
+  InvalidRequestError,
+  SlotAlreadyVacantError
+};
